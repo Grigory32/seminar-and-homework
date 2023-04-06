b31922f94df9b0d79d94085c84a21a6737fd059d
Grigory Kachurovskiy <grigorirubik@mail.ru>
2023-04-06 23:44:15 +0900

    1 seminar

---
+++ seminar 1/Program.cs
@@ -0,0 +1,59 @@
+﻿// int number1 = 5;
+// double number2 = 12,65
+
+// string word = "my word!";
+// char symbol = '1';
+
+// bool check = true;
+// bool check = false;
+
+// int num1 = 5;
+// int num2 = 7;
+// Console.WriteLine("my numbers are " + num1 + " and " + num2 + " and it is good!");
+// Console.WriteLine($"My numbers are {num1} and {num2} and it is good!");
+
+// Console.Write("input a number: ");
+// int num = Convert.ToInt32 (Console.ReadLine()); Convert.ToInt32 преобразует string в int информация которую вводит пользователь считывается как string
+
+// Console.WriteLine("Your number is " + num);
+
+
+
+
+// Homework 1. 
+
+// Task 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+
+// Console.Write("input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num2 * num2;
+
+// if (num1 == quad2)
+// {
+//     Console.WriteLine("Yes!");
+// }
+
+// else
+// { 
+//   Console.WriteLine("No!");  
+// }
+
+
+// Task 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("input a positive number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current  = -num;
+
+// while(current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+//
