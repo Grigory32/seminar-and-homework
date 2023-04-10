e332e4f0a4c5a13ae41b30ac823de808d7a9c097
Grigory Kachurovskiy <grigorirubik@mail.ru>
2023-04-10 15:21:27 +0900

    domashka

---
+++ dzSamoprowerka1/Program.cs
@@ -0,0 +1,51 @@
+﻿// task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int CutNumber(int num)
+// {
+//     int result = num / 10 % 10;
+    
+//     return result;
+// }
+
+
+// Console.Write("input a three digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int ex = 100;
+// int ux = 999; 
+
+// if (num < ex)
+//     Console.WriteLine("you lose");
+ 
+   
+//  if (num > ux)  
+//      Console.WriteLine("lose");
+ 
+       
+// int answer = CutNumber(num);
+
+// Console.WriteLine($"введённое число {num} ответ {answer}");
+
+
+
+// task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+//  bool Chill(int day)
+//  {
+//     if (day == 6 || day == 7)
+//         return true;
+//     else 
+//         return false;
+
+//  }
+
+// Console.WriteLine("Введите день: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Chill(day));
+
+
+
+// task 3. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
